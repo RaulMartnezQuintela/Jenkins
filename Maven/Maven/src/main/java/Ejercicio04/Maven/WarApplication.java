af19b5a66c078d16b21c8373b9b60e9380347ebf
RaulMartnezQuintela <135589083+RaulMartnezQuintela@users.noreply.github.com>
2023-06-07 10:13:56 +0200

    Add files via upload

---
+++ Maven/Maven/src/main/java/Ejercicio04/Maven/WarApplication.java
@@ -0,0 +1,13 @@
+package Ejercicio04.Maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarApplication.class, args);
+	}
+
+}
---
+++ Maven/Maven/src/test/java/Ejercicio04/Maven/WarApplicationTests.java
@@ -0,0 +1,13 @@
+package Ejercicio04.Maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
